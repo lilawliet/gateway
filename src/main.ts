b52b03198da5d5b3af42968998fff635daa2265c
DESKTOP-ST0GILC\w2517 <25175731@qq.com>
2024-03-21 00:56:00 +0800

    fastify & 版本控制 & 全局返回参数格式

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // 接口版本化管理
+  app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+    type: VersioningType.URI,
+  });
+
+  // 统一响应体格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
