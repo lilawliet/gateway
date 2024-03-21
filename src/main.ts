5e5ac4c3ce8489459f0fc94745d36ac1c29b3f06
DESKTOP-ST0GILC\w2517 <25175731@qq.com>
2024-03-22 00:12:26 +0800

    异常拦截

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import {
 } from '@nestjs/platform-fastify';
 import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/interceptors/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/interceptors/exceptions/http.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -21,6 +23,9 @@ async function bootstrap() {
 
   // 统一响应体格式
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  // 异常过滤器
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
 
   await app.listen(3000);
 }
